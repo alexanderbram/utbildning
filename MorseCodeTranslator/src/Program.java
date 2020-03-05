9b0803c5280456f00e3fc3ab7f629177b104b99f
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-03-05 15:55:13 +0100

    hej

--- MorseCodeTranslator/src/Program.java
+++ MorseCodeTranslator/src/Program.java
@@ -7,37 +7,50 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String newText = "";
-		
 		char[] letters = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 				'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-				'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+				'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0','.',',','?'};
 		
-		String[] morseLetters = {"    ", ". ___", "___ . . .", "___ . ___ .", "___ . .", ".", ". . ___ .", "___ ___ .",
-				 ". . . .", ". .", ". ___ ___ ___", "___ . ___", ". ___ . .", "___ ___", "___ .",
-				 "___ ___ ___", ". ___ ___ .", "___ ___ . ___", ". ___ .", ". . .", "_", ". . ___",
-				 ". . . ___", ". ___ ___", "___ . . ___", "___ . ___ ___", "___ ___ . .",
-				 ". ___ ___ ___ ___", ". . ___ ___ ___", ". . . ___ ___", ". . . . ___",
-				 ". . . . .", "___ . . . .", "___ ___ . . .", "___ ___ ___ . .", "___ ___ ___ ___ .",
-				 "___ ___ ___ ___ ___"};
+		String[] morseLetters = {"   ", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+				 "....", "..", ".---", "-.-", ".-..", "--", "-.",
+				 "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+				 "...-", ".--", "-..-", "-.--", "--..",
+				 ".----", "..---", "...--", "....-",
+				 ".....", "-....", "--...", "---..", "----.",
+				 "-----",".-.-.-","--..--","..--.."};
 		
+		System.out.println("Text to translate to morse: ");
 		String textConvert = input.nextLine();
 		char[] ch = new char[textConvert.length()];
 		for (int x = 0; x < textConvert.length(); x++) {
 			ch[x] = textConvert.charAt(x);
 		}
 		for (int i = 0; i < ch.length; i++) {
-			for (int j = 0; j < 37; j++) {
+			for (int j = 0; j < 40; j++) {
 				if (ch[i] == letters[j]) {
 					newText += morseLetters[j];
-					newText += "   ";
+					newText += "  ";
 					break;
 				}
 			}
 		}
 		System.out.println("Text in morse: ");
 		System.out.println(newText);
-
 		
+		System.out.println("Text to split: ");
+		String test = input.nextLine();
+		String[] testArray = test.split(" ");
+		String newMorseString = "";
+		for (String object : testArray) {
+			System.out.println(object);
+			for (int i = 0; i < 40; i++) {
+				if (object == morseLetters[i]) {
+					newMorseString += letters[i];
+				}
+			}
+		}
+		System.out.println("Morse in text: ");
+		System.out.println(newMorseString);
 	}
 
 }
